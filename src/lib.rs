dddf4e7eefd3abbce5d33fcf90b19cb7cd7cc8d0
Edgar Luque <git@edgarluque.com>
2022-02-02 14:46:49 +0100

    upd cargo toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-//!
 //! [![Version](https://img.shields.io/crates/v/nompdf)](https://crates.io/crates/nompdf)
 //! [![Downloads](https://img.shields.io/crates/d/nompdf)](https://crates.io/crates/nompdf)
 //! [![License](https://img.shields.io/crates/l/nompdf)](https://crates.io/crates/nompdf)
 //! ![Rust](https://github.com/edg-l/nompdf/workflows/Rust/badge.svg)
 //! [![Docs](https://docs.rs/nompdf/badge.svg)](https://docs.rs/nompdf)
 //!
-//! ## A PDF parser written in Rust and using nom.
+//! Work in progress.
+//!
+//! ## A PDF parser written in Rust using nom.
 //!
 //! Using [PDF Reference third edition](https://www.adobe.com/content/dam/acom/en/devnet/pdf/pdfs/pdf_reference_archives/PDFReference.pdf) as reference.
 
